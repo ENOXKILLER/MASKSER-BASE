9b80728d30467483a7b20d973f6e1aa19d8dd658
MASK-SER <148147631+ENOXKILLER@users.noreply.github.com>
2024-09-20 05:14:16 -0700

    Update Reboot.js

--- bmw/Reboot.js
+++ bmw/Reboot.js
@@ -19,7 +19,7 @@ const{repondre,ms,dev,superUser}=com;
 
   const {exec}=require("child_process")
 
-    repondre("BMW-MD bot Restarting ⏳");
+    repondre("MASKSER-MD bot Restarting ⏳");
 
   exec("pm2 restart all");
   
