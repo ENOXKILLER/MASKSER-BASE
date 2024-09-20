323b3c03c98bf2f67073a9c6cec2f05d719a4786
MASK-SER <148147631+ENOXKILLER@users.noreply.github.com>
2024-09-20 05:20:30 -0700

    Update anime.js

--- bmw/anime.js
+++ bmw/anime.js
@@ -130,9 +130,9 @@ zokou({
     const response = await axios.get(`http://www.omdbapi.com/?apikey=742b2d09&t=${arg}&plot=full`);
     const imdbData = response.data;
 
-    let imdbInfo = "Tap on the link to join movie channel on telegram and download movies there : https://t.me/ibrahimtechai\n";
-    imdbInfo += " ``` BMW MD FILMS```\n";
-    imdbInfo += "*Made by Ibrahim Adams*\n";
+    let imdbInfo = "Tap on the link to join movie channel on telegram and download movies there : https://t.me/MaskserTech\n";
+    imdbInfo += " ``` MASKSER MD FILMS```\n";
+    imdbInfo += "*Made by Maskser Tech*\n";
     imdbInfo += "🎬Title    : " + imdbData.Title + "\n";
     imdbInfo += "📅year      : " + imdbData.Year + "\n";
     imdbInfo += "⭐Assessment : " + imdbData.Rated + "\n";
